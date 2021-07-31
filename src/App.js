f209311a7e189c71d664db7f2c70dcd4d37e8f53
menezesmario <mario082@gmail.com>
2021-07-30 21:58:32 -0300

    fundamentos do react - componentes, estados e propriedades

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+  const usuario = useState('Mario')
   return (
+    <>
     <input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuário" />
+    <button type="button">Pesquisar</button>
+    </>  
   );
 }
 
 export default App;
+
+
+// [ usuario, setUsuario ]
